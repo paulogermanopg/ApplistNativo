eafcf5b87187bfb00965ad7adbeb45e693c02429
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-05-26 13:52:55 -0300

    ok

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { ScrollView, View, Text, StyleSheet} from 'react-native'
 import { DrawerItems } from 'react-navigation-drawer'
 import { Gravatar } from 'react-native-gravatar'
+import CommonStyles from '../commonStyles'
 
 export default props => {
     return (
         <ScrollView>
             <View style={styles.header}>
+                <Text style={styles.title}>AppList</Text>
                 <Gravatar style={styles.avatar}
                     options={{
                         email: props.navigation.getParam('email'),
@@ -14,8 +16,12 @@ export default props => {
                     }} />
             </View>
             <View style={styles.userInfo}>
-                <Text>{props.navigation.getParam('name')}</Text>
-                <Text>{props.navigation.getParam('email')}</Text>
+                <Text style={styles.name}>
+                    {props.navigation.getParam('name')}
+                </Text>
+                <Text style={styles.email}>
+                    {props.navigation.getParam('email')}
+                </Text>
             </View>
             <DrawerItems {...props} />
         </ScrollView>
@@ -33,6 +39,29 @@ const styles = StyleSheet.create({
         borderWidth: 3,
         borderRadius: 30,
         margin: 15,
+        backgroundColor: '#222',
         marginTop: 20,
+    },
+    title:{
+        color: '#000',
+        fontFamily: CommonStyles.fontfamily,
+        fontSize: 30,
+        paddingTop: 30,
+        padding: 10,
+    },
+    userInfo: {
+        marginLeft: 10,
+    },
+    name: {
+        fontFamily: CommonStyles.fontfamily,
+        fontSize: 20,
+        color: CommonStyles.color.mainText,
+        marginBottom: 5,
+    },
+    email: {
+        fontFamily: CommonStyles.fontfamily,
+        fontSize: 15,
+        color: CommonStyles.color.subText,
+        marginBottom: 10,
     }
 })
